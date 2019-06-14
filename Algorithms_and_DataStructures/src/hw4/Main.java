2b37393ff32fc0b17d07e9df60831c5c71db7047
AndrewSukhobok95 <suha1253516@gmail.com>
2019-06-14 18:15:56 +0300

    hw4_cont

--- Algorithms_and_DataStructures/src/hw4/Main.java
+++ Algorithms_and_DataStructures/src/hw4/Main.java
@@ -3,6 +3,18 @@ package hw4;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Check MyLinkedList");
+
+        MyLinkedList<Integer> mll = new MyLinkedList<>();
+        mll.insertLast(4);
+        mll.insertFirst(3);
+        mll.insertFirst(2);
+        mll.insertFirst(1);
+        mll.insertLast(5);
+        mll.insertFirst(0);
+        mll.insertLast(6);
+
+        System.out.println(mll);
+
         System.out.println("==================");
     }
 }
--- Algorithms_and_DataStructures/src/hw4/MyLinkedList.java
+++ Algorithms_and_DataStructures/src/hw4/MyLinkedList.java
@@ -37,18 +37,48 @@ public class MyLinkedList<T> {
         last=null;
     }
 
-    public boolean isEmpty() {
-        return first==null;
-    }
+    public boolean isEmpty() { return first==null; }
 
     public void insertFirst(T value){
         Node newNode = new Node(value);
         newNode.setNext(first);
-        if (isEmpty()){
-            first = newNode;
+        newNode.setPrevious(null);
+        if (isEmpty()) {
             last = newNode;
         }else{
-            newNode.setPrevious(first);
+            first.setPrevious(newNode);
         }
+        first = newNode;
+        size++;
+    }
+
+    public void insertLast(T value){
+        Node newNode = new Node(value);
+        newNode.setNext(null);
+        newNode.setPrevious(last);
+        if (isEmpty()) {
+            first = newNode;
+        }else{
+            last.setNext(newNode);
+        }
+        last = newNode;
+        size++;
+    }
+
+    @Override
+    public String toString() {
+        Node current = first;
+        String sep;
+        String s = "";
+        while (current != null) {
+            if (current.getNext()==null){
+                sep = "";
+            }else{
+                sep = ", ";
+            }
+            s += current.getValue() + sep;
+            current = current.getNext();
+        }
+        return s;
     }
 }
